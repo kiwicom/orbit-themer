6f9b61cdcce0ca433d1a9fc13746144af917ec16
mainframev <vgenaev@gmail.com>
2022-07-27 13:41:49 +0200

    chore: bump deps and fix flow errors

--- src/Components/Alert/index.js
+++ src/Components/Alert/index.js
@@ -1,39 +1,47 @@
 // @flow
-import React from "react"
-import styled from "styled-components"
-import Alert from "@kiwicom/orbit-components/lib/Alert"
+import * as React from "react";
+import styled from "styled-components";
+import Alert from "@kiwicom/orbit-components/lib/Alert";
 
 const StyledAlerts = styled.div`
   display: flex;
-  
+
   > div {
     width: 100%;
-    
+
     + div {
       margin-left: 16px;
     }
   }
-`
+`;
 
-const Alerts = () => {
-
+const Alerts = (): React.Node => {
   const message = "Type longer explanation of your message here...";
 
   return (
     <StyledAlerts>
       <div>
-        <Alert type="info" title="Test">{message}</Alert>
+        <Alert type="info" title="Test">
+          {message}
+        </Alert>
       </div>
       <div>
-        <Alert type="success" title="Test">{message}</Alert>
+        <Alert type="success" title="Test">
+          {message}
+        </Alert>
       </div>
       <div>
-        <Alert type="warning" title="Test">{message}</Alert>
+        <Alert type="warning" title="Test">
+          {message}
+        </Alert>
       </div>
       <div>
-        <Alert type="critical" title="Alert">{message}</Alert>
+        <Alert type="critical" title="Alert">
+          {message}
+        </Alert>
       </div>
     </StyledAlerts>
-)}
+  );
+};
 
-export default Alerts
+export default Alerts;
--- src/Components/index.js
+++ src/Components/index.js
@@ -1,74 +1,68 @@
 // @flow
-import React from "react"
-import styled from "styled-components"
-import Alert from "./Alert"
-import Badge from "./Badge"
-import Button from "./Button"
-import ButtonLink from "./ButtonLink"
-import Checkbox from "./Checkbox"
-import InputField from "./InputField"
-import Card from "@kiwicom/orbit-components/lib/Card"
-import CardHeader from "@kiwicom/orbit-components/lib/Card/CardHeader"
-import CardSection from "@kiwicom/orbit-components/lib/Card/CardSection"
+import * as React from "react";
+import styled from "styled-components";
+import Alert from "./Alert";
+import Badge from "./Badge";
+import Button from "./Button";
+import ButtonLink from "./ButtonLink";
+import Checkbox from "./Checkbox";
+import InputField from "./InputField";
+import Card from "@kiwicom/orbit-components/lib/Card";
+import CardSection from "@kiwicom/orbit-components/lib/Card/CardSection";
 
 const StyledComponentsOuter = styled.div`
-  background: #F5F7F9;
+  background: #f5f7f9;
   padding: 0 32px;
   max-height: 100vh;
   flex: 1 auto;
   overflow-y: scroll;
-`
+`;
 
 const StyledComponentsInner = styled.div`
   padding: 16px 0;
-  
-   > div {
+
+  > div {
     margin-top: 16px;
     height: auto;
   }
-`
+`;
 
-const Components = () => (
+const Components = (): React.Node => (
   <StyledComponentsOuter>
     <StyledComponentsInner>
-      <Card>
-          <CardHeader title="Alerts" />
-          <CardSection>
-              <Alert />
-          </CardSection>
+      <Card title="Alerts">
+        d{" "}
+        <CardSection>
+          <Alert />
+        </CardSection>
       </Card>
-      <Card>
-          <CardHeader title="Badges"/>
-          <CardSection>
-            <Badge />
-          </CardSection>
+      <Card title="Badges">
+        <CardSection>
+          <Badge />
+        </CardSection>
       </Card>
-      <Card>
-        <CardHeader title="Buttons"/>
+      <Card title="Buttons">
         <CardSection>
           <Button />
         </CardSection>
       </Card>
-      <Card>
-        <CardHeader title="ButtonLinks"/>
+      <Card title="ButtonLinks">
         <CardSection>
           <ButtonLink />
         </CardSection>
       </Card>
-      <Card>
-        <CardHeader title="Checkboxes"/>
+      <Card title="Checkboxes">
         <CardSection>
           <Checkbox />
         </CardSection>
       </Card>
-      <Card>
-        <CardHeader title="InputFields"/>
+      <Card title="InputFields">
         <CardSection>
           <InputField />
         </CardSection>
       </Card>
     </StyledComponentsInner>
   </StyledComponentsOuter>
-)
+);
 
-export default Components
+export default Components;
